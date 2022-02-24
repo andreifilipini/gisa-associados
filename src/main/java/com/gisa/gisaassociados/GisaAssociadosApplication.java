a02e0e12fda220c13ab1e658f129a332ae7e3788
Andrei Filipini <andrei.filipini@sigma.com.br>
2022-02-23 23:15:53 -0300

    add swagger

--- src/main/java/com/gisa/gisaassociados/GisaAssociadosApplication.java
+++ src/main/java/com/gisa/gisaassociados/GisaAssociadosApplication.java
@@ -1,8 +1,10 @@
 package com.gisa.gisaassociados;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication(scanBasePackages = "com.gisa")
 public class GisaAssociadosApplication {
 
